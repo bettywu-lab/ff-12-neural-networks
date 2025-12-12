9d212dd1764550ae255d7427e4046a9ce4054899
bettywu-lab <queen.bettywu@gmail.com>
2025-12-12 10:34:13 -0500

    Update start.py

--- start.py
+++ start.py
@@ -2,4 +2,5 @@ import numpy as np
 from sklearn.datasets import make_moons # simple datatset for training the model
 import matplotlib.pyplot as plt
 
+pip install -r requirements.txt
 # plot the dataset below!
